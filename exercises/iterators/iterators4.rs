21b7066bbeb8baacba7159f53fbfe2d0dccac9e1
Patryk Kuklinski <patryk.kuklinski@nordicsemi.no>
2023-06-13 08:24:35 +0200

    Smart pointers done

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,7 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +12,18 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    //num.iter().fold(0, |acc, x| acc * x)
+
+// Standard recursion algorithm
+    // if num == 0 {
+    //     1
+    // }else {
+    //     factorial(num - 1) * num
+    // }
+
+    (1..num + 1).fold(1, |sum, x| sum * x)
+    
 }
 
 #[cfg(test)]
